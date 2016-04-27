ec4e7bf009d36256b1e4d041c96b7669082c82e8
Peter Jang <peter.x.jang@gmail.com>
2016-04-27 15:59:38 -0500

    meeting index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/' => 'meetings#index'
+
+  get '/meetings' => 'meetings#index'
+  get '/meetings/:id' => 'meetings#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
