3ed23dae493a05d80037d7b085babc87b495e105
Peter Jang <peter.x.jang@gmail.com>
2016-08-22 20:04:23 -0500

    Get data via angular  (not using it yet)

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -1,8 +1,10 @@
 /* global angular */
 
 (function() {
-  angular.module('app').controller('meetingsCtrl', function($scope) {
-    $scope.message = "Goodbye world";
+  angular.module('app').controller('meetingsCtrl', function($scope, $http) {
+    $http.get('/api/v1/meetings.json').then(function(response) {
+      console.log(response.data);
+    });
 
     window.$scope = $scope;
   });
