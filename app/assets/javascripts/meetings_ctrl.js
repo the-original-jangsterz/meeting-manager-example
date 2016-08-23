44f4463579e5acdc960582d960c4135dedc776b7
Peter Jang <peter.x.jang@gmail.com>
2016-08-22 20:13:28 -0500

    Display meeting data using Angular instead of Rails

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -3,7 +3,7 @@
 (function() {
   angular.module('app').controller('meetingsCtrl', function($scope, $http) {
     $http.get('/api/v1/meetings.json').then(function(response) {
-      console.log(response.data);
+      $scope.meetings = response.data;
     });
 
     window.$scope = $scope;
